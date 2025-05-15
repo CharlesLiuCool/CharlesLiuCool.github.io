66c297e0966c083ed702359da0ed8698bc263259
CharlesLiuCool <148991486+CharlesLiuCool@users.noreply.github.com>
2025-05-14 22:50:31 -0700

    improved projects layout and UI

--- src/lib/components/Projects/ProjectItem.svelte
+++ src/lib/components/Projects/ProjectItem.svelte
@@ -3,16 +3,31 @@
 	export let award_class: string;
 	export let description: string;
 	export let link: string;
+	export let imageURL: string;
+
+	let hovering = false;
 </script>
 
-<div class="project-item">
-	<h2>{title}</h2>
-	<!--Award/Class-->
-	<h3>{award_class}</h3>
-	<p>{description}</p>
-	{#if link}
-		<div class="project-details">
+<div 
+  class="project-item"
+  on:mouseenter={() => hovering = true}
+  on:mouseleave={() => hovering = false}
+  role="group"
+>
+	<div class="project-text">
+		<h2>{title}</h2>
+		<h3>{award_class}</h3>
+		<p>{description}</p>
+		{#if link}
+			<div class="project-details">
 			<a href={link}>More Details...</a>
-		</div>
-	{/if}
+			</div>
+		{/if}
+	</div>
+	<img
+		src={imageURL}
+		alt={`Screenshot or image of project titled ${title}`}
+		class="hover-image"
+		class:visible={hovering}
+	/>
 </div>
--- src/lib/components/Projects/Projects.svelte
+++ src/lib/components/Projects/Projects.svelte
@@ -1,16 +1,53 @@
 <script lang="ts">
 	import ProjectItem from './ProjectItem.svelte';
+	import { onMount } from 'svelte';
 
 	const projects = [
-		{ title: 'Snowpack-prediction Artificial Intelligence - SAI', award_class: '1st place 2025 AgAID Institute Agath0n Winner', description: 'Machine learning algorithm using SciKit optimize\'s random forest to predict snow-water equivalent values based on real geospatial and meteorological data with 96%+ NSE.', link: '/SAI' },
-		{ title: 'Supply Station Optimizer - Optimizer\'s Inc.', award_class: 'WSU MATH 464 - Linear Optimization (Dr. Tom Asaki)', description: 'Formulated linear optimization model to optimally place supply stations in Manhattan-style road grid. Factored in neighborhood populations and prevented placement on existing neighborhoods. Used Python, Pandas, and SciPy.', link: '/OptimizersInc' },
-		{ title: 'DecoSavvy', award_class: '3rd place WSU CrimsonCode Hackathon', description: 'Used Unity\'s AR Foundation to build a visual reality mobile application that lets users visualize furniture/decor before in the real world before making purchase decisions. Paired with custom Svelte website that let users shop for goods, mimicking online retail platforms.', link: '/DecoSavvy' },
+		{
+			title: 'Snowpack-prediction Artificial Intelligence - SAI',
+			award_class: '1st place 2025 AgAID Institute Agath0n Winner',
+			description: 'Machine learning algorithm using SciKit optimize\'s random forest to predict snow-water equivalent values based on real geospatial and meteorological data with 96%+ NSE.',
+			link: '/SAI',
+			imageURL: '/images/SAI_cover.jpg'
+		},
+		{
+			title: 'Supply Station Optimizer - Optimizer\'s Inc.',
+			award_class: 'WSU MATH 464 - Linear Optimization (Dr. Tom Asaki)',
+			description: 'Formulated linear optimization model to optimally place supply stations in Manhattan-style road grid. Factored in neighborhood populations and prevented placement on existing neighborhoods. Used Python, Pandas, and SciPy.',
+			link: '/OptimizersInc',
+			imageURL: '/images/SAI_cover.jpg'
+		},
+		{
+			title: 'DecoSavvy',
+			award_class: '3rd place WSU CrimsonCode Hackathon',
+			description: 'Used Unity\'s AR Foundation to build a visual reality mobile application that lets users visualize furniture/decor before in the real world before making purchase decisions. Paired with custom Svelte website that let users shop for goods, mimicking online retail platforms.',
+			link: '/DecoSavvy',
+			imageURL: '/images/SAI_cover.jpg'
+		},
 	];
+
+	let titleVisible = false;
+	let titleElement: HTMLElement;
+
+	//projects fade animation
+	onMount(() => {
+		const observer = new IntersectionObserver(
+			([entry]) => {
+				titleVisible = entry.isIntersecting;
+			},
+			//if 10% or more the word is showing, do animation
+			{ threshold: 0.1 }
+		);
+
+		if (titleElement) observer.observe(titleElement);
+
+		return () => observer.disconnect(); // Clean up on destroy
+	});
 </script>
 
 <section id="projects">
 	<div class="projects">
-		<h1>PROJECTS</h1>
+		<h1 bind:this={titleElement} class:fade-in-visible={titleVisible}>PROJECTS</h1>
 		{#each projects as project}
 			<ProjectItem {...project} />
 		{/each}
