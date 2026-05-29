96438735372f144e4b8c6d39d9d2f1f8c6456a03
CharlesLiuCool <148991486+CharlesLiuCool@users.noreply.github.com>
2026-05-28 23:24:45 -0700

    fix github pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
 export default defineConfig({
+  site: 'https://charlesliucool.github.io',
   integrations: [
     mdx({
       remarkPlugins: [remarkMath],
